af529d91563dba761a68772ae298f93c7a81d50d
lobsterick <lobsterick@gmail.com>
2018-11-23 00:27:03 +0100

    Register models to admin panel

--- movie_api/admin.py
+++ movie_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Movie, Comment, Rating
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Comment)
+admin.site.register(Rating)
