a0a2f348520585da00667cf932a10cb5fc363260
lobsterick <lobsterick@gmail.com>
2018-11-23 21:04:41 +0100

    Add Heroku files

--- OMDB_API_bridge/settings.py
+++ OMDB_API_bridge/settings.py
@@ -124,4 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+import django_heroku
+django_heroku.settings(locals(), test_runner=False)
